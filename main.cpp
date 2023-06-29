b2a6271913c649337affe19da91feaf255f96b98
Pedro Abritta <pedroabritta98@gmail.com>
2023-06-29 10:09:43 -0400

    TAS-12/first_commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <mutex>
 #include "first_MC_sim.hpp"
+#include "statistics.hpp"
 
 std::mutex mtx;
 
@@ -11,7 +11,7 @@ int main(int argc, char* argv[])
     //read in variables -- make sure they are correct
     try {
         if (argc != ARG_COUNT + 1){
-            //printf("You have entered %d argument(s), and this program needs %d: \n \n   1 - \n   2 - \n   3 - \n   4 - \n\n\n", argc, ARG_COUNT);
+            cout << "You have entered the wrong number of argument(s). This program needs: \n \n   1 - N = number of samples \n   2 - n = number of iterations/sample \n   3 - radius = radius of a circumference inscribed in the square/area \n   4 - hole_count = number of holes being tested \n   5 - thread_count = number of threads activated in multithreading\n\n";
             throw std::runtime_error("Sorry! You have entered the wrong number of arguments. Review your entries and try again :) - The MC team\n");
         }
         N = stoi(argv[1]);
@@ -28,7 +28,8 @@ int main(int argc, char* argv[])
     //parallelization variables
     std::vector< std::thread > threads;
     vector < MC_run > runs;
-    vector < double > final_probability(thread_count);
+    //vector < double > final_probability(thread_count);
+    vector < vector < double > > final_probability(thread_count);
     int N_prime;
     bool set_up = true;
     while(set_up){
@@ -45,6 +46,7 @@ int main(int argc, char* argv[])
     
     for (int i = 0; i < thread_count; ++i) {
             runs.emplace_back(N_prime, n, radius, hole_count, i);
+            final_probability[i].resize(N_prime);
     }
 
     for (size_t i = 0; i < runs.size(); ++i) {
@@ -56,16 +58,16 @@ int main(int argc, char* argv[])
         thread.join();
     }
     cout << "\n\n";
-    double value = 0.0;
-    for (int i = 0; i < final_probability.size(); i++) {
-        value += 100.0*final_probability[i]/(N_prime*n);
-        cout << "Sample " << i << " had "  << final_probability[i] << " falls\n";
-    }
-    cout << "\n\nProbability of finding a hole: " << value << " percent\n";
+
+
+    // Stats Object 
+    Stats stats_obj(thread_count, N, n, final_probability);
+    stats_obj.Worker();
+
     auto finish = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<std::chrono::microseconds>(finish - start).count();
     float exec_time = float(duration)/1000000.0;
-    cout << "Execution took " << exec_time << " seconds\n";
+    cout << "\n\nExecution took " << exec_time << " seconds\n\n";
 
     return 0;
 }
